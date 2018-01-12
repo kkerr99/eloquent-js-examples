965ec1e6a445f1834b198fc3a4232d2898a4d195
Kris Kerr <kkerr99@gmail.com>
2018-01-11 18:04:38 -0800

    Added 3.2 'Recursion'

--- ch3.1.js
+++ ch3.1.js
@@ -40,3 +40,72 @@ function min(x, y) {
 console.log(min(1, 3));
 console.log(min(3, 1));
 console.log(min(3, 3));
+/*
+  3.2a Recursion
+  Here's another way to determine if a positive whole number (>= 0) is
+  even or odd:
+    -- Zero is even
+    -- One is odd
+    -- For any other number N, its evenness is the same as (N-2)
+  Define a recursive function 'isEven' corresponding to this description.
+  The function should accept a 'number' parameter and return a Boolean.
+
+  Tests
+    50 -> true
+    75 -> false
+    -1 -> Stack overflow
+          Why? Can you think of a way to fix this?
+        -- Test if argument is negative
+*/
+function isEven(number) {
+  if (number == 0) {
+    return true;
+  }
+  else if (number == 1) {
+    return false;
+  }
+  else {
+    return isEven(number-2);
+  }
+}
+
+// Test harness
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+
+/*
+  3.2b Recursion
+  Works for negative numbers as well
+  Tests
+    50  -> true
+    75  -> false
+    -1  -> false
+    -50 -> true
+    -75 -> false
+
+*/
+function isEven(number) {
+  if (number == 0) {
+    return true;
+  }
+  else if ((number == 1) || (number == -1)) {
+    return false;
+  }
+  else {
+    // For positive numbers
+    if (Math.sign(number) == 1) {
+      return isEven(number-2);
+    }
+    else if (Math.sign(number) == -1) {
+        return isEven(number+2);
+    }
+  }
+}
+
+// Test harness
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+console.log(isEven(-50));
+console.log(isEven(-75));
