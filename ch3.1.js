e2f740c5d9e06cc6dd87fa6b31d6e8e38ec7afa4
Kris Kerr <kkerr99@gmail.com>
2018-01-11 18:11:39 -0800

    Added skeleton code for 3.a and 3.b

--- ch3.1.js
+++ ch3.1.js
@@ -109,3 +109,25 @@ console.log(isEven(75));
 console.log(isEven(-1));
 console.log(isEven(-50));
 console.log(isEven(-75));
+
+/*
+  3.3a Bean Counting
+  Write a function "countBs" that takes a string as its only argument and
+  returns a number that indicates how many uppercase "B" characters are in
+  the string.
+
+  Tests
+
+*/
+
+/*
+  3.3b Bean Counting
+  Write a function called "countChar" that behaves like "countBs", except it
+  takes a second argument that indicates the character that is to be counted
+  (rather than counting only uppercase "B" characters).
+
+  Rewrite "countBs" to make use of this new function.
+
+  Tests
+
+*/
