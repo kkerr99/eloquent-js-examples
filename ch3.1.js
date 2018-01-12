e910fa15d5da880ff7d9cb476d96c7408c2f5948
Kris Kerr <kkerr99@gmail.com>
2018-01-11 17:33:08 -0800

    Added 3.1 Minimum

--- ch3.1.js
+++ ch3.1.js
@@ -1,3 +1,5 @@
+/* Working through the book (and all exercises) a second time */
+
 // Example of running out of call stack space ("blowing the call stack")
 function chicken() {
   return egg();
@@ -8,3 +10,33 @@ function egg() {
 }
 
 console.log(chicken() + "came first.");
+
+
+/*
+  3.1 Minimum
+  Write a function 'min' that takes two arguments and returns the minimum
+  of the two.
+
+  Tests
+
+  1, 3 --> 1
+  3, 1 --> 1
+  3, 3 --> null
+*/
+function min(x, y) {
+  if (x < y) {
+    return x;
+  }
+  else if (y < x) {
+    return y;
+  }
+  // if x = y
+  else {
+    return null;
+  }
+}
+
+// Test harness
+console.log(min(1, 3));
+console.log(min(3, 1));
+console.log(min(3, 3));
