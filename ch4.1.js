f257290a4944eb48d9d2a50dc76e831957e8e86a
Kris Kerr <kkerr99@gmail.com>
2018-01-16 17:57:23 -0800

    Added more Weresquirrel functionality

--- ch4.1.js
+++ ch4.1.js
@@ -136,3 +136,16 @@ function tableFor(event, journal) {
 }
 
 console.log(tableFor("pizza", JOURNAL));
+
+var map = {};
+function storePhi(event, phi) {
+  map[event] = phi;
+}
+
+storePhi("pizza", 0.069);
+storePhi("touched tree", -0.081);
+console.log("pizza" in map);
+console.log(map["touched tree"]);
+
+for (var event in map)
+  console.log("The correlation for '" + event + "' is " + map[event]);
