bdc9763b52f748e63b7c76707e0f57efb56e5f3d
Kris Kerr <kkerr99@gmail.com>
2018-01-31 18:51:57 -0800

    Add some more JSON fun

--- ch5.js
+++ ch5.js
@@ -48,3 +48,20 @@ console.log(JSON.parse(string).born);
 
 var ancestry = JSON.parse(ANCESTRY_FILE);
 console.log(ancestry.length);
+
+function filter(array, test) {
+  var passed = [];
+  for (var i = 0; i < array.length; i++) {
+    if (test(array[i]))
+      passed.push(array[i]);
+  }
+  return passed;
+}
+
+console.log(filter(ancestry, function(person) {
+  return person.born > 1900 && person.born < 1925;
+}));
+
+console.log(ancestry.filter(function(person) {
+  return person.father == "Carel Haverbeke";
+}));
