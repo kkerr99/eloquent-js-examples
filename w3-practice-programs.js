1ad43ca2ec0c47961763fb264e9a72d3143cacda
Kris Kerr <kkerr99@gmail.com>
2018-02-05 21:24:43 -0800

    Add bubbleSort()

--- w3-practice-programs.js
+++ w3-practice-programs.js
@@ -111,3 +111,40 @@ function isPrime(num) {
   }
   return positiveDivisor;
 }
+
+/*
+24. Write a JavaScript function to apply Bubble Sort algorithm.
+Note : According to wikipedia "Bubble sort, sometimes referred to as sinking
+sort, is a simple sorting algorithm that works by repeatedly stepping
+through the list to be sorted, comparing each pair of adjacent items and
+swapping them if they are in the wrong order".
+
+Sample array : [12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213]
+Expected output : [3223, 546, 455, 345, 234, 213, 122, 98, 84, 64, 23, 12, 9, 4, 1]
+*/
+function bubbleSort(array) {
+  var length = array.length;
+  // Print unsorted array
+  console.log("Unsorted: " + array);
+  // Passes through array
+  for (i = 0; i < length-1; i++) {
+    var x = 0;
+    var y = 0;
+    // Do adjacent pair swaps
+    for (j = 0; j < length - 1; j++)
+    {
+      x = array[j];
+      y = array[j+1];
+      // If x > y, swap array pair
+      if (x > y) {
+        arr[j] = y;
+        arr[j+1] = x;
+      }
+    }
+  }
+  // Print array
+  console.log("Sorted: " + array);
+}
+
+var arr = [12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213];
+bubbleSort(arr);
