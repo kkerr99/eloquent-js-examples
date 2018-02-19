752be7c5a79c6830b3b3be191ee2e177742324fe
Kris Kerr <kkerr99@gmail.com>
2018-02-18 17:23:45 -0800

    Project Euler #16--Lattice paths

--- w3-practice-programs.js
+++ w3-practice-programs.js
@@ -1511,7 +1511,7 @@ console.log(longestCollatzSequence(UPPERLIMIT));
 /*
 https://projecteuler.net/problem=15
 
-Lattice paths
+#15--Lattice paths
 
 Starting in the top left corner of a 2×2 grid, and only being able to move to the
 right and down, there are exactly 6 routes to the bottom right corner.
@@ -1544,5 +1544,42 @@ function numPaths(gridLimit) {
 
 
 var GRIDLIMIT = 2;
+console.log(numPaths(GRIDLIMIT));
+// 6
+
+/*
+https://projecteuler.net/problem=15
+
+#16--Lattice paths
+
+Starting in the top left corner of a 2×2 grid, and only being able to move to the
+right and down, there are exactly 6 routes to the bottom right corner.
+
+How many such routes are there through a 20×20 grid?
+
+*/
+
+function numPaths(gridLimit) {
+  var STARTX = 0;
+  var STARTY = 0;
+
+  function mapGrid(x, y) {
+    if ((x == gridLimit) && (y == -gridLimit)) {
+      return 1;
+    }
+    else if (x == gridLimit) {
+      return mapGrid(x, y-1);
+    }
+    else if (y == -gridLimit) {
+      return mapGrid(x+1, y);
+    }
+    else {
+      return mapGrid(x+1, y) + mapGrid(x, y-1);
+    }
+  }
+  return mapGrid(STARTX, STARTY);
+}
+
+var GRIDLIMIT = 20;
 console.log(numPaths(GRIDLIMIT));
 // 6
